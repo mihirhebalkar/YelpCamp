869e8e3b67bd987df91b620e4b1dc879e30cc611
mihirhebalkar <mihirhebalkar07@gmail.com>
2024-12-27 19:52:47 +0530

    Setup express-session and flash messages using partials.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
+const session = require('express-session');
+const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const catchAsync = require('./utils/catchAsync');
 
@@ -22,12 +24,33 @@ db.once("open", () => {
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.urlencoded({ extended: true }));
 
 app.use(methodOverride('_method'));
 
-app.use(express.static(path.join(__dirname, 'public')));
+const sessionConfig = {
+    secret : 'secretkey',
+    resave : false,
+    saveUninitialized : true,
+    cookie : {
+        httpOnly : true,
+        expires : Date.now() + 1000*60*60*24*7,
+        maxAge : 1000*60*60*24*7
+    }
+}
+
+app.use(session(sessionConfig));
+
+app.use(flash());
+
+app.use((req,res,next)=> {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+
 
 app.use('/campgrounds', campgrounds);
 app.use('/campgrounds/:id/reviews', reviews)
