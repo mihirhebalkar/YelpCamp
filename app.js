8b7c77d636bba03498b7ddda13896d5e51591226
mihirhebalkar <mihirhebalkar07@gmail.com>
2024-12-27 19:04:31 +0530

    Restructure app.js by creating a routes folder.

--- app.js
+++ app.js
@@ -2,14 +2,12 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-const Campground = require('./models/campground');
 const methodOverride = require('method-override');
 const ExpressError = require('./utils/ExpressError');
 const catchAsync = require('./utils/catchAsync');
-const { title } = require('process');
-const {campgroundSchema, reviewSchema} = require('./schemas');
-const campground = require('./models/campground');
-const Review = require('./models/review');
+
+const campgrounds = require('./routes/campground');
+const reviews = require('./routes/review');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp',{
     useNewURLParser: true,
@@ -32,89 +30,12 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(methodOverride('_method'));
 
-const validateCampground = (req, res, next) => {
-    const { error } = campgroundSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
-    }
-}
-
-const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body);
-
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
-    }
-}
+app.use('/campgrounds', campgrounds);
+app.use('/campgrounds/:id/reviews', reviews)
 
 app.get('/', (req, res) => {
     res.render('home');
 })
-
-app.get('/campgrounds', catchAsync(async (req, res) => {
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', { campgrounds });
-}))
-
-app.get('/campgrounds/new', catchAsync(async(req, res)=> {
-    res.render('campgrounds/new');
-}))
-
-app.post('/campgrounds',validateCampground, catchAsync(async(req, res) => {
-
-    // if(!req.body.campground) throw new ExpressError('Invalid campground data',400);
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-}))
-
-app.get('/campgrounds/:id', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate('reviews');
-    res.render('campgrounds/show', {campground});
-}))
-
-app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    res.render('campgrounds/edit', {campground});
-}))
-
-app.put('/campgrounds/:id',validateCampground, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id,{ ...req.body.campground });
-    res.redirect(`/campgrounds/${campground._id}`);
-}))
-
-app.delete('/campgrounds/:id', catchAsync(async(req, res) => {
-    const { id } = req.params;
-    await Campground.findByIdAndDelete(id);
-    res.redirect('/campgrounds');
-}))
-
-//Reviews routes
-
-app.post('/campgrounds/:id/reviews', validateReview,catchAsync(async(req,res)=>{
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-}))
-
-
-app.delete('/campgrounds/:id/reviews/:reviewId',catchAsync(async(req,res)=>{
-    const {reviewId, id} = req.params;
-    await Campground.findByIdAndUpdate(id, {$pull: { reviews: reviewId }});
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/campgrounds/${id}`);
-}))
-
 
 app.all('*',(req, res,next) => {
     next(new ExpressError('Page not found',404))
@@ -125,7 +46,6 @@ app.use((err,req,res,next) => {
     if(!err.message) err.message = 'Something went wrong!'
     res.status(statusCode).render('error', { err });
 })
-
 
 app.listen(3000, ()=> {
     console.log("Serving on port 3000"); 
